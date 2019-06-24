6eddcfdd525355f12a5030c77612c5fbefd288cf
Serena Dominguez <serenadominguez@gmail.com>
2019-06-24 17:41:32 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+// TITLE PAGE 
+// window.addEventListener("keyup", ev => {
+//   if (ev.keyCode === 38) {
+//     document.body.innerHTML = "*"
+//   }
+// });
+// $(function() {
+//   $(".draggable").draggable();
+//   $(".droppable").droppable({
+//     drop: function(event, ui) {
+//       $(this)
+//         .addClass("ui-state-highlight")
+//         .find("p")
+//         .html("Dropped!");
+//     }
+//   });
+// });
+
+
+  // $("").draggable();
+
+}); //end
