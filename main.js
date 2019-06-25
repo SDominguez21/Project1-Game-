d8ab63219effc9ac5586629eaf2d38fa9a993cae
Serena Dominguez <serenadominguez@gmail.com>
2019-06-24 23:26:27 -0400

    progress

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
-$(document).ready(function(){
-// TITLE PAGE 
-// window.addEventListener("keyup", ev => {
-//   if (ev.keyCode === 38) {
-//     document.body.innerHTML = "*"
-//   }
-// });
-// $(function() {
-//   $(".draggable").draggable();
-//   $(".droppable").droppable({
-//     drop: function(event, ui) {
-//       $(this)
-//         .addClass("ui-state-highlight")
-//         .find("p")
-//         .html("Dropped!");
-//     }
-//   });
-// });
+$(document).ready(function() {
+  // // TITLE PAGE
+  // window.addEventListener("keyup", ev => {
+  //   if (ev.keyCode === 38) {
+  //     document.body.innerHTML = "*"
+  //   }
+  // });
 
-
-  // $("").draggable();
-
+  $(function() {
+    $(".draggable").draggable();
+    $(".droppable").droppable({
+      drop: function(event, ui) {
+        $(this)
+          .addClass("ui-state-highlight")
+          .find("p")
+          .html("Dropped!");
+      }
+    });
+  });
 }); //end
